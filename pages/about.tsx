93dfc38d7ad797066c6d4327a0e3fc0616e87e1f
ebowwa <81942069+ebowwa@users.noreply.github.com>
2024-01-09 23:44:08 -0800

    Update about.tsx

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,1 +1,13 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About Page</h1>
+      {/* Additional content goes here */}
+    </div>
+  );
+};
+
+export default About;
 
