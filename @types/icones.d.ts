d9225c7fb1a52ce2faa521d9904c9f9b6a447bc2
ebowwa <81942069+ebowwa@users.noreply.github.com>
2024-01-10 15:41:14 -0800

    Create icones.d.ts

---
+++ @types/icones.d.ts
@@ -0,0 +1,5 @@
+// icones.d.ts
+declare module '@icones/codicon/hubot';
+declare module '@icones/radix-icons/discord-logo';
+declare module '@icones/codicon/terminal-linux';
+declare module '@icones/cryptocurrency/gto';
