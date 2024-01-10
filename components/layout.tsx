ad4bb42f895a85ffd47acc4024df0e578b32bc1a
ebowwa <81942069+ebowwa@users.noreply.github.com>
2024-01-09 23:05:52 -0800

    Update layout.tsx

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,18 +1,30 @@
 import Footer from './footer'
 import Meta from './meta'
+import React from 'react'
+import ReactMarkdown from 'react-markdown'
+import { Alert } from './alert' // Import your custom component
 
 type Props = {
   preview?: boolean
   children: React.ReactNode
 }
 
+const renderers = {
+  image: () => <Footer/>,
+  // Map the custom component to the corresponding Markdown element
+  customComponentIMade: () => <CustomComponentIMade/>
+};
+
 const Layout = ({ preview, children }: Props) => {
   return (
     <>
       <Meta />
       <div className="min-h-screen">
-        <main>{children}</main>
-//       <Alert />
+        <main>
+          {/* Pass the renderers to the ReactMarkdown component */}
+          <ReactMarkdown source={children} renderers={renderers} />
+        </main>
+        {/* <Alert /> */}
       </div>
       <Footer />
     </>
