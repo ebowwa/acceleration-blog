c84c902a58b89b6b5f6316f3b4566c7b67d4d288
ebowwa <81942069+ebowwa@users.noreply.github.com>
2024-01-10 21:05:23 -0800

    Update curls.tsx

--- pages/curls.tsx
+++ pages/curls.tsx
@@ -1,44 +1,21 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
-const CurlResultsPage: React.FC = () => {
-  const [searchResults, setSearchResults] = useState<string>('');
-  const [allResults, setAllResults] = useState<string>('');
-  const [sortResults, setSortResults] = useState<string>('');
-
-  useEffect(() => {
-    // Perform the first curl command
-    fetch('https://acceleration-e14gqajju-ebowwa.vercel.app/api/posts/search/searchPosts?keywords[]=JavaScript&keywords[]=API')
-      .then((response) => response.text())
-      .then((data) => setSearchResults(data))
-      .catch((error) => console.error(error));
-
-    // Perform the second curl command
-    fetch('https://acceleration-iofvxqzaf-ebowwa.vercel.app/api/posts/all')
-      .then((response) => response.text())
-      .then((data) => setAllResults(data))
-      .catch((error) => console.error(error));
-
-    // Perform the third curl command
-    fetch('https://acceleration-e14gqajju-ebowwa.vercel.app/api/posts/sorting/sortPosts?sortBy=title&order=asc')
-      .then((response) => response.text())
-      .then((data) => setSortResults(data))
-      .catch((error) => console.error(error));
-  }, []);
-
+const CurlPage: React.FC = () => {
   return (
     <div>
-      <h1>Curl Results Page</h1>
-      
-      <h2>Search Results:</h2>
-      <pre>{searchResults}</pre>
+      <h1>Curl Commands</h1>
+      <pre>
+        {`# Search Posts
+curl -X GET 'https://acceleration-e14gqajju-ebowwa.vercel.app/api/posts/search/searchPosts?keywords[]=JavaScript&keywords[]=API'
 
-      <h2>All Results:</h2>
-      <pre>{allResults}</pre>
+# Get All Posts
+curl https://acceleration-iofvxqzaf-ebowwa.vercel.app/api/posts/all
 
-      <h2>Sort Results:</h2>
-      <pre>{sortResults}</pre>
+# Sort Posts by Title (Ascending)
+curl -X GET 'https://acceleration-e14gqajju-ebowwa.vercel.app/api/posts/sorting/sortPosts?sortBy=title&order=asc'`}
+      </pre>
     </div>
   );
 };
 
-export default CurlResultsPage;
+export default CurlPage;
