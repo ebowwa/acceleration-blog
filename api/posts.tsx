d6b6944ba644aae32630fd2c5a9b2b8e4ed8b8f9
ebowwa <81942069+ebowwa@users.noreply.github.com>
2024-01-11 02:20:50 +0000

    up6

--- api/posts.tsx
+++ api/posts.tsx
@@ -1,26 +1,31 @@
 // api/posts.ts
 
-import fs from 'fs';
+import fs from 'fs/promises';
 import path from 'path';
+import matter from 'gray-matter';
 
-export default (req, res) => {
-  // Define the path to your _posts directory
-  const postsDirectory = path.join(process.cwd(), '_posts');
+export default async (req, res) => {
+  try {
+    // Define the path to your _posts directory
+    const postsDirectory = path.join(process.cwd(), '_posts');
 
-  // Read all the markdown files in the _posts directory
-  const fileNames = fs.readdirSync(postsDirectory);
+    // Read all the file names in the _posts directory
+    const fileNames = await fs.readdir(postsDirectory);
 
-  // Read the content of each markdown file and include it in the JSON response
-  const postsData = fileNames.map((fileName) => {
-    const fullPath = path.join(postsDirectory, fileName);
-    const fileContents = fs.readFileSync(fullPath, 'utf8');
+    // Read the content of each markdown file and convert it to JSON
+    const postsData = await Promise.all(
+      fileNames.map(async (fileName) => {
+        const fullPath = path.join(postsDirectory, fileName);
+        const fileContents = await fs.readFile(fullPath, 'utf8');
+        const { data, content } = matter(fileContents);
+        return { ...data, content };
+      })
+    );
 
-    return {
-      fileName: fileName.replace('.md', ''), // Include the file name without the .md extension
-      content: fileContents, // Include the raw content of the markdown file
-    };
-  });
-
-  // Respond with the JSON data
-  res.status(200).json(postsData);
+    // Respond with the JSON data
+    res.status(200).json(postsData);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 };
