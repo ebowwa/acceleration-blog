deb176e581f31851317cbad407473026ccfef0e4
ebowwa <81942069+ebowwa@users.noreply.github.com>
2024-01-14 23:06:09 +0000

    next.config.cjs update

--- next.config.cjs
+++ next.config.cjs
@@ -1,6 +1,11 @@
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+});
 const withNextra = require('nextra')({
-    theme: 'nextra-theme-blog',
-    themeConfig: './theme.config.js'
-  })
-  
-  module.exports = withNextra()
+  theme: 'nextra-theme-blog',
+  themeConfig: './theme.config.js'
+});
+
+module.exports = withMDX(withNextra({
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx']
+}));
