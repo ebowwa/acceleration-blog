4ed8f27ee6d9d2234c3e5f222c5e50baaa4faab2
ebowwa <81942069+ebowwa@users.noreply.github.com>
2024-01-15 10:47:39 +0000

    merge

--- components/post-body.tsx
+++ components/post-body.tsx
@@ -8,7 +8,15 @@ type Props = {
 const PostBody = ({ content }: Props) => {
   return (
     <div className="max-w-2xl mx-auto">
-      <ReactMarkdown className={markdownStyles['markdown']} children={content} />
+      <ReactMarkdown 
+        className={markdownStyles['markdown']} 
+        children={content} 
+        components={{
+          h1: ({node, ...props}) => <h1 className="text-4xl mt-12 mb-6 leading-tight" {...props} />,
+          h2: ({node, ...props}) => <h2 className="text-3xl mt-12 mb-4 leading-snug" {...props} />,
+          // Add similar components for h3, h4, etc. if needed
+        }}
+      />
     </div>
   )
 }
