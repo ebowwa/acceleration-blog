66cb60022c60676be3609bc6160e799015beace9
ebowwa <81942069+ebowwa@users.noreply.github.com>
2024-01-15 10:27:43 +0000

    post

--- components/post-body.tsx
+++ components/post-body.tsx
@@ -1,4 +1,5 @@
-import markdownStyles from './markdown-styles.module.css'
+import ReactMarkdown from 'react-markdown';
+import markdownStyles from './markdown-styles.module.css';
 
 type Props = {
   content: string
@@ -7,12 +8,9 @@ type Props = {
 const PostBody = ({ content }: Props) => {
   return (
     <div className="max-w-2xl mx-auto">
-      <div
-        className={markdownStyles['markdown']}
-        dangerouslySetInnerHTML={{ __html: content }}
-      />
+      <ReactMarkdown className={markdownStyles['markdown']} children={content} />
     </div>
   )
 }
 
-export default PostBody
+export default PostBody;
